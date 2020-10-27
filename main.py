d81402a98bbd2c4f86b8666971b336c4a29c40b9
Taner Durmaz <47399791+closedAi@users.noreply.github.com>
2020-10-27 15:44:40 +0300

    Update main.py
    shuffling improved, now shuffles with  exact 10 random moves

--- main.py
+++ main.py
@@ -4,29 +4,35 @@ import random
 def mixer(puzzleGrid):
   emptyX = 3
   emptyY = 3
-  for x in range(100):
+  count = 0
+  while count < 10:
     rand = random.randint(0, 3)
-    if rand == 0 and emptyX - 1 > 0 :
+    if rand == 0 and emptyX - 1 >= 0 :
       puzzleGrid[emptyX][emptyY] = puzzleGrid[emptyX - 1][emptyY]
       puzzleGrid[emptyX - 1][emptyY] = 0
       emptyX -= 1
+      count += 1
     elif rand == 1 and emptyY + 1 < 4 :
       puzzleGrid[emptyX][emptyY] = puzzleGrid[emptyX][emptyY + 1]
       puzzleGrid[emptyX][emptyY + 1] = 0
       emptyY += 1
+      count += 1
     elif rand == 2 and emptyX + 1 < 4 :
       puzzleGrid[emptyX][emptyY] = puzzleGrid[emptyX + 1][emptyY]
       puzzleGrid[emptyX + 1][emptyY] = 0 
       emptyX += 1
+      count += 1
 
-    elif rand == 3 and emptyY - 1 > 0 :
+    elif rand == 3 and emptyY - 1 >= 0 :
       puzzleGrid[emptyX][emptyY] = puzzleGrid[emptyX][emptyY - 1]
       puzzleGrid[emptyX][emptyY - 1] = 0
       emptyY -= 1
-
-    print()  
-    print("decision is " + str(rand))
-    print()  
+      count += 1
+    #debug
+    #print()  
+    #print("decision is " + str(rand))
+    #print("count is " + str(count))
+    #print()  
     #printer(puzzleGrid)
 
 def printer(Array):
@@ -37,7 +43,7 @@ def printer(Array):
 T = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 5], [4, 5, 5, 0]]
 
 printer(T)
-
+print()
 mixer(T)
 
 printer(T)
